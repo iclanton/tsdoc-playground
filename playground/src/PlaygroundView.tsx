36ddf3f4608f8549a39389e3bb5c7881eea2235c
pgonzal <pgonzal@users.noreply.github.com>
2018-10-07 12:38:28 -0700

    Set up refresh timer

--- playground/src/PlaygroundView.tsx
+++ playground/src/PlaygroundView.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+// import * as tsdoc from '@microsoft/tsdoc';
 
 interface IPlaygroundViewProps {
 }
@@ -9,6 +10,9 @@ interface IPlaygroundViewState {
 }
 
 export class PlaygroundView extends React.Component<IPlaygroundViewProps, IPlaygroundViewState>  {
+  private _reparseTimerHandle: number | undefined = undefined;
+  private _reparseNeeded: boolean = true;
+
   constructor(props: IPlaygroundViewProps, context?: any) { // tslint:disable-line:no-any
     super(props, context);
 
@@ -16,6 +20,17 @@ export class PlaygroundView extends React.Component<IPlaygroundViewProps, IPlayg
       inputText: '',
       outputText: ''
     };
+  }
+
+  public componentDidMount(): void {
+    this._reparseTimerHandle = setInterval(this._reparseTimer_onTick.bind(this), 2000);
+  }
+
+  public componentWillUnmount(): void {
+    if (this._reparseTimerHandle !== undefined) {
+      clearInterval(this._reparseTimerHandle);
+      this._reparseTimerHandle = undefined;
+    }
   }
 
   public render(): React.ReactNode {
@@ -44,8 +59,24 @@ export class PlaygroundView extends React.Component<IPlaygroundViewProps, IPlayg
 
   private _inputTextArea_onChange(event: React.ChangeEvent<HTMLTextAreaElement>): void {
     this.setState({
-      inputText: event.target.value,
-      outputText: event.target.value
+      inputText: event.target.value
     });
+    this._reparseNeeded = true;
+  }
+
+  private _reparseTimer_onTick(): void {
+    if (!this._reparseNeeded) {
+      return;
+    }
+    this._reparseNeeded = false;
+    try {
+      this.setState({
+        outputText: this.state.inputText
+      });
+    } catch (error) {
+      this.setState({
+        outputText: 'Unhandled exception: ' + error.message
+      });
+    }
   }
 }
