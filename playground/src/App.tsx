93f874df60a59be5c78f6b35e3efb3ac0ea0bfaa
pgonzal <pgonzal@users.noreply.github.com>
2018-10-07 13:02:32 -0700

    Initial working parser loop

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import './App.css';
 import { PlaygroundView } from './PlaygroundView';
 
 class App extends React.Component {
   public render(): React.ReactNode {
 
     return (
-      <div className='App'>
+      <div>
         <h1>TSDoc Playground</h1>
 
         <PlaygroundView />
--- playground/src/PlaygroundView.tsx
+++ playground/src/PlaygroundView.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-// import * as tsdoc from '@microsoft/tsdoc';
+import * as tsdoc from '@microsoft/tsdoc';
 
 interface IPlaygroundViewProps {
 }
@@ -7,6 +7,7 @@ interface IPlaygroundViewProps {
 interface IPlaygroundViewState {
   inputText: string;
   outputText: string;
+  errorsText: string;
 }
 
 export class PlaygroundView extends React.Component<IPlaygroundViewProps, IPlaygroundViewState>  {
@@ -17,13 +18,28 @@ export class PlaygroundView extends React.Component<IPlaygroundViewProps, IPlayg
     super(props, context);
 
     this.state = {
-      inputText: '',
-      outputText: ''
+      inputText:
+        [
+          '/**',
+          ' * Returns the average of two numbers.',
+          ' *',
+          ' * @remarks',
+          ' * This method is part of the {@link core-library#Statistics | Statistics subsystem}.',
+          ' *',
+          ' * @param x - The first input number',
+          ' * @param y - The second input number',
+          ' * @returns The arithmetic mean of `x` and `y`',
+          ' *',
+          ' * @beta',
+          ' */'
+        ].join('\n'),
+      outputText: '',
+      errorsText: ''
     };
   }
 
   public componentDidMount(): void {
-    this._reparseTimerHandle = setInterval(this._reparseTimer_onTick.bind(this), 2000);
+    this._reparseTimerHandle = setInterval(this._reparseTimer_onTick.bind(this), 700);
   }
 
   public componentWillUnmount(): void {
@@ -37,6 +53,10 @@ export class PlaygroundView extends React.Component<IPlaygroundViewProps, IPlayg
     const textAreaStyle: React.CSSProperties = {
       width: '600px',
       height: '400px'
+    };
+    const errorsTextAreaStyle: React.CSSProperties = {
+      width: '1200px',
+      height: '200px'
     };
 
     return (
@@ -52,6 +72,15 @@ export class PlaygroundView extends React.Component<IPlaygroundViewProps, IPlayg
           readOnly={ true }
           value={ this.state.outputText }
           style={ textAreaStyle }
+          />
+        <br />
+        Errors:
+        <br />
+        <textarea
+          id='errors-textarea'
+          readOnly={ true }
+          value={ this.state.errorsText }
+          style={ errorsTextAreaStyle }
           />
       </div>
     );
@@ -70,13 +99,41 @@ export class PlaygroundView extends React.Component<IPlaygroundViewProps, IPlayg
     }
     this._reparseNeeded = false;
     try {
+      const inputText: string = this.state.inputText;
+      const tsdocParser: tsdoc.TSDocParser = new tsdoc.TSDocParser();
+      const parserContext: tsdoc.ParserContext = tsdocParser.parseString(inputText);
+
+      const errorsText: string = parserContext.log.messages.map(x => x.toString()).join('\n');
+
+      const outputLines: string[] = [];
+      if (parserContext.docComment) {
+        this._dumpTSDocTree(outputLines, parserContext.docComment);
+      }
+
       this.setState({
-        outputText: this.state.inputText
+        outputText: outputLines.join('\n'),
+        errorsText
       });
     } catch (error) {
       this.setState({
-        outputText: 'Unhandled exception: ' + error.message
+        outputText: '',
+        errorsText: 'Unhandled exception: ' + error.message
       });
+    }
+  }
+
+  private _dumpTSDocTree(outputLines: string[], docNode: tsdoc.DocNode, indent: string = ''): void {
+    let dumpText: string = `${indent}- ${docNode.kind}`;
+    if (docNode instanceof tsdoc.DocNodeLeaf && docNode.excerpt) {
+      const content: string = docNode.excerpt.content.toString();
+      if (content.length > 0) {
+        dumpText += ': ' + JSON.stringify(content);
+      }
+    }
+    outputLines.push(dumpText);
+
+    for (const child of docNode.getChildNodes()) {
+      this._dumpTSDocTree(outputLines, child, indent + '  ');
     }
   }
 }
