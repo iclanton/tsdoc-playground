0abb8bf212e6722823d0df1eb15e1ca563b8ce5a
Ian Clanton-Thuon <iclantonthuon@outlook.com>
2018-10-07 20:57:18 -0700

    Setting up basic bundling.

---
+++ playground/build.js
@@ -0,0 +1,10 @@
+'use strict';
+
+debugger;
+
+const productionIndex = process.argv.indexOf('--production');
+if (productionIndex !== -1) {
+  process.argv[productionIndex] = '--env.production';
+}
+
+require('webpack-cli/bin/cli');
---
+++ playground/webpack.config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const { generateBuildWebpackConfiguration } = require('./webpack.shared.config');
+
+module.exports = function(env) {
+  debugger;
+  return generateBuildWebpackConfiguration(env);
+}
